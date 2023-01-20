db490c66e335e0de6a232c98b170b9870debf0df
JokerPSYF <todorvasilev005@gmail.com>
2023-01-20 16:41:24 +0200

    ++

---
+++ DatingApp/API/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController : Controller
+    {
+        private readonly DataContext context;
+
+        public UserController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            return user;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
